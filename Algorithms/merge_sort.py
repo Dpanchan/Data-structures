86d92047ab938be4354fcfaced1c633d77d9fee5
Divya <dpanchan@kent.edu>
2019-09-15 12:27:05 -0400

    Create merge_sort.py

---
+++ Algorithms/merge_sort.py
@@ -0,0 +1,34 @@
+def merge_sort(a):
+  if len(a) > 1:
+    mid = len(a) //2
+    left, right = a[:mid], a[mid:]
+    merge_sort(left)
+    merge_sort(right)
+
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+      if left[i] < right[j]:
+        a[k] = left[i]
+        i += 1
+      else:
+        a[k] = right[j]
+        j += 1
+      k += 1
+
+    while i < len(left):
+      a[k] = left[i]
+      i += 1
+      k += 1
+    
+    while j < len(right):
+      a[k] = right[j]
+      j += 1
+      k += 1
+    
+a = [9,3,2,7,1,0]
+merge_sort(a)
+print(a)
+
+# space complexity : 
+# time complexity : 
